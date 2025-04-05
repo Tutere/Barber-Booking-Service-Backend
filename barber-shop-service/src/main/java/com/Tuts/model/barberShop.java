38e2e8a2aabe45b17c19ecf7df051dce09f2e0ae
tuteredurie <tuteredurie@hotmail.com>
2025-04-05 15:46:54 +1300

    feat: added barber shop model

---
+++ barber-shop-service/src/main/java/com/Tuts/model/barberShop.java
@@ -0,0 +1,50 @@
+package com.Tuts.model;
+
+import java.time.LocalTime;
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.Getter;
+
+@Entity
+@Data // Lombok will generate getters, setters, toString, equals, hashCode methods and
+      // constructors
+public class barberShop {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @ElementCollection
+    private List<String> images;
+
+    @Column(nullable = false)
+    private String address;
+
+    @Column(nullable = false)
+    private String phoneNumber;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String city;
+
+    @Column(nullable = false)
+    private Long ownerId;
+
+    @Column(nullable = false)
+    private LocalTime openingTime;
+
+    @Column(nullable = false)
+    private LocalTime closingTime;
+}
