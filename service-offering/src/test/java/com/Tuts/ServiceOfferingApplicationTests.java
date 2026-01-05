007c19997d887b435568ebae4bf03eecf05f26a5
tuteredurie <tuteredurie@hotmail.com>
2026-01-05 15:42:34 +1300

    init service offering service

---
+++ service-offering/src/test/java/com/Tuts/ServiceOfferingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Tuts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceOfferingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
