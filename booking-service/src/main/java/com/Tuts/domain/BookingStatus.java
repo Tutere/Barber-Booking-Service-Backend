86c9d953699fadf9a8489b580e758ff781833d01
tuteredurie <tuteredurie@hotmail.com>
2026-01-06 16:18:17 +1300

    add booking model, booking status enum and lombok package to pom.xml

---
+++ booking-service/src/main/java/com/Tuts/domain/BookingStatus.java
@@ -0,0 +1,7 @@
+package com.Tuts.domain;
+
+public enum BookingStatus {
+    PENDING,
+    CONFIRMED,
+    CANCELLED
+}
---
+++ booking-service/src/main/java/com/Tuts/model/Booking.java
@@ -0,0 +1,37 @@
+package com.Tuts.model;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+
+import com.Tuts.domain.BookingStatus;
+
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class Booking {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public Long id;
+
+    private Long salonId;
+
+    private Long customerId;
+
+    private LocalDateTime startTime;
+
+    private LocalDateTime endTime;
+
+    @ElementCollection
+    private Set<Long> serviceOfferingIds;
+
+    private BookingStatus status = BookingStatus.PENDING;
+
+    private int totalPrice;
+}
